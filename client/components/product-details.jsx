b3f70ee9a8b5a7d3be806ff4020edf56e023032f
blake-ros <dev@MSI.localdomain>
2020-03-10 16:07:47 -0700

    updated product details render method that now has the template for how the product details should be laid out

--- client/components/product-details.jsx
+++ client/components/product-details.jsx
@@ -19,9 +19,25 @@ class ProductDetails extends React.Component {
   }
 
   render() {
+    // const myProduct = this.state.product.map(product => {
+    //   const newNumber = product.price / 100;
+    //   const newerNumber = Number(newNumber).toFixed(2);
+
     return (
       <div className="container-fluid">
-
+        <div className="row">
+          <img className="col-6" src={this.props.img} alt="product image"></img>
+          <div className="col-6">
+            <div className="card-body">
+              <h1 className="mb-3">{this.props.name}</h1>
+              <span className="mb-3">{this.props.price}</span>
+              <p>{this.props.shortDescription}</p>
+            </div>
+          </div>
+        </div>
+        <div className="row">
+          <p>{this.props.longDescription}</p>
+        </div>
       </div>
     );
   }
