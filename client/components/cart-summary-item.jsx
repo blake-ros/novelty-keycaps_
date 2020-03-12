baa1ca99b38d8cb3b723712f2710823edb32b355
blake-ros <dev@MSI.localdomain>
2020-03-12 14:48:33 -0700

    refactored cart summary item component

--- client/components/cart-summary-item.jsx
+++ client/components/cart-summary-item.jsx
@@ -2,17 +2,19 @@ import React from 'react';
 
 class CartSummaryItem extends React.Component {
   render() {
+    const cartItem = this.props.item;
+
     return (
       <div className="card mb-3" style="max-width: 540px;">
         <div className="row no-gutters">
           <div className="col-md-4">
-            <img src={this.item.image} alt="product-image"></img>
+            <img src={cartItem.image} className="card-img" alt="product-image"></img>
           </div>
           <div className="col-md-8">
             <div className="card-body">
-              <h2 className="card-title">{this.item.name}</h2>
-              <span className="card-text">{this.item.price}</span>
-              <span className="card-text">{this.item.shortDescription}</span>
+              <h2 className="card-title">{cartItem.name}</h2>
+              <span className="card-text">{cartItem.price}</span>
+              <span className="card-text">{cartItem.shortDescription}</span>
             </div>
           </div>
         </div>
--- client/components/cart-summary.jsx
+++ client/components/cart-summary.jsx
@@ -4,12 +4,20 @@ import CartSummaryItem from './cart-summary-item.jsx';
 
 class CartSummary extends React.Component {
   render() {
+    const cartArray = this.state.cart;
     return (
       <div className="container-fluid">
         <Header cartItemCount={this.state.cart.length}/>,
         <button onClick={() => this.props.onRender('catalog', {})}> &lt; Back to Catalog</button>
         <h1>My Cart</h1>,
-        {CartSummaryItem},
+        {
+          cartArray.length === 0
+        ? <h1>No Items In Cart</h1>
+        : cartArray.map(item => {
+          return <CartSummaryItem key={item.productId} item={item}
+        });
+      }
+        // {CartSummaryItem},
         <h1>Item Total {total}</h1>
       </div>
     );
