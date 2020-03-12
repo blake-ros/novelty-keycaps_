05e5116439aeb50a85d2dde3379cfff877500ee4
blake-ros <dev@MSI.localdomain>
2020-03-12 13:08:52 -0700

    added render method to cart summary to correctly layout the cart items

--- client/components/cart-summary.jsx
+++ client/components/cart-summary.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import Header from './header.jsx';
+import CartSummaryItem from './cart-summary-item.jsx';
 
 class CartSummary extends React.Component {
   render() {
     return (
-      <Header cartItemCount={this.state.cart.length}/>
+      <div className="container-fluid">
+        <Header cartItemCount={this.state.cart.length}/>,
+        <h1>My Cart</h1>,
+        {CartSummaryItem},
+        <h1>Item Total {total}</h1>
+      </div>
     );
   }
 }
