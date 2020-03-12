63eb5b81b7a8bc5aa684dc5dc61b4a40660e2299
blake-ros <dev@MSI.localdomain>
2020-03-12 10:34:24 -0700

    defined get cart items method, added cartItemCount to header componenet, defined addtocart method in app component

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -9,9 +9,11 @@ export default class App extends React.Component {
     this.state = {
       message: null,
       isLoading: true,
-      view: { name: 'catalog', params: {} }
+      view: { name: 'catalog', params: {} },
+      cart: []
     };
     this.setView = this.setView.bind(this);
+    this.getCartItems = this.getCartItems.bind(this);
   }
 
   componentDidMount() {
@@ -20,12 +22,33 @@ export default class App extends React.Component {
       .then(data => this.setState({ message: data.message || data.error }))
       .catch(err => this.setState({ message: err.message }))
       .finally(() => this.setState({ isLoading: false }));
+
+    this.getCartItems();
   }
 
   setView(name, params) {
     this.setState({
       view: { name, params }
     });
+  }
+
+  getCartItems() {
+    fetch('/api/cart')
+      .then(res => res.json())
+      .catch(err => this.setState({ message: err.message }));
+  }
+
+  addToCart(product) {
+    fetch('/api/cart', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(product)
+    })
+      .then(response => response.json())
+      .then(data => {
+        const myCart = this.state.cart;
+        this.setState({ cart: myCart.concat(data) });
+      });
   }
 
   render() {
--- client/components/header.jsx
+++ client/components/header.jsx
@@ -5,6 +5,7 @@ class Header extends React.Component {
     return (
       <div>
         <h1 className="text-center bg-dark"><i className="fas fa-dollar-sign"></i> Wicked Sales <i className="fas fa-dollar-sign"></i></h1>
+        <h2 className="text-right">{cartItemCount} Items<i className="fas fa-shopping-cart"></i></h2>
       </div>
     );
   }
