2a8f20bcdc7b11aa055921279db629ffe2a56311
blake-ros <dev@MSI.localdomain>
2020-03-13 13:01:21 -0700

    added checkout form component with initial constructor and state object

---
+++ client/components/checkout-form.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class CheckoutForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: null,
+      creditCard: null
+    };
+  }
+
+  render() {
+    return(
+
+    )
+  }
+}
+
+export default CheckoutForm;
