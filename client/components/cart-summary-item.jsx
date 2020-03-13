0b74683a967f32c365c1533ade3c3969dfc7e3c2
blake-ros <dev@MSI.localdomain>
2020-03-13 10:23:52 -0700

    pdated conditional rendering for if a cart is empty

--- client/components/cart-summary-item.jsx
+++ client/components/cart-summary-item.jsx
@@ -3,10 +3,9 @@ import React from 'react';
 class CartSummaryItem extends React.Component {
   render(props) {
     const cartItem = this.props.cartItem;
-    console.log(cartItem);
 
     return (
-      <div className="card mb-3" style={{ maxWidth: '540 px' }}>
+      <div className="card mb-3 mt-3" style={{ maxWidth: '540 px' }}>
         <div className="row no-gutters">
           <div className="col-md-4">
             <img src={cartItem.image} className="card-img" alt="product-image"></img>
@@ -14,8 +13,8 @@ class CartSummaryItem extends React.Component {
           <div className="col-md-8">
             <div className="card-body">
               <h2 className="card-title">{cartItem.name}</h2>
-              <span className="card-text">{cartItem.price}</span>
-              <span className="card-text">{cartItem.shortDescription}</span>
+              <span className="card-text text-secondary mb-5">${(cartItem.price * 0.01).toFixed(2)}</span>
+              <p className="card-text mt-3">{cartItem.shortDescription}</p>
             </div>
           </div>
         </div>
--- client/components/cart-summary.jsx
+++ client/components/cart-summary.jsx
@@ -13,12 +13,17 @@ class CartSummary extends React.Component {
 
   render(props) {
     const cartArray = this.props.cart;
-    console.log(cartArray);
 
+    let itemsInCart;
+    if (cartArray.length === 0) {
+      itemsInCart = <h1>No Items In Cart</h1>;
+    } else {
+      itemsInCart = <h1>My Cart</h1>;
+    }
     return (
       <div className="container-fluid">
         <button onClick={() => this.props.onRender('catalog', {})}>&lt; Back to Catalog</button>
-        <h1>My Cart</h1>
+        {itemsInCart}
         {cartArray.map(item => { return <CartSummaryItem key={item.cartItemId} cartItem={item}/>; })}
         {/* <button onClick={() => this.props.onRender('catalog', {})}> &lt; Back to Catalog</button>
         <h1>My Cart</h1>,
