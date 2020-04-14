fc4f7efffed22a1e7faff0e992f9fc7a7f4ac965
blake-ros <dev@MSI.localdomain>
2020-04-14 13:41:21 -0700

    added a sponsors section

---
+++ client/components/sponsors.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+class Sponsors extends React.Component {
+  render(props) {
+    return (
+      <div className="page-header pt-3">
+        <h1 className="text-center testimonial">Sponsors</h1>
+        <div className="row d-flex justify-content-between">
+          <div className="col-2"><img src="./images/ducky_thumb.png"></img></div>
+          <div className="col-2"><img src="./images/vortex_thumb.png"></img></div>
+          <div className="col-2"><img></img></div>
+          <div className="col-2"><img></img></div>
+          <div className="col-2"><img></img></div>
+          <div className="col-2"><img></img></div>
+        </div>
+        <div className="page-header">stuff here</div>
+      </div>
+    );
+  }
+}
+
+export default Sponsors;
