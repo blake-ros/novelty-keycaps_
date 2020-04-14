49fce9a3814e9f61df489e4a981fa464294e2e5e
blake-ros <dev@MSI.localdomain>
2020-04-14 12:49:56 -0700

    changed header gradient and added testimonial

--- client/components/header.jsx
+++ client/components/header.jsx
@@ -2,9 +2,10 @@ import React from 'react';
 
 class Header extends React.Component {
   render() {
+    const testimonial = 'The Ultimate Keycap Catalog';
     return (
       <div className="page-header">
-        <h1 className="text-center bg-dark pb-5 pt-5">Novelty Keycaps_</h1>
+        <h1 className="text-center pb-5 pt-5">Novelty Keycaps_<h6 className="text-center testimonial">&quot;{testimonial}&quot;</h6></h1>
         <div className="d-flex justify-content-end col-12"><h2 className="text-right pointer col-xl-2 col-lg-3 col-md-4 col-sm-4 col-xs-4 pr-4" onClick={() => this.props.onRender('cart', {})}>{this.props.cartItemCount} Items  <i className="fas fa-shopping-cart"></i></h2></div>
       </div>
     );
