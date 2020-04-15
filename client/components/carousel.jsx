627bcf86757c73ef37baf35f7812420c1b140941
blake-ros <dev@MSI.localdomain>
2020-04-15 10:54:23 -0700

    added next button to carousel component

--- client/components/carousel.jsx
+++ client/components/carousel.jsx
@@ -24,6 +24,10 @@ class Carousel extends React.Component {
           <span className="carousel-control-prev-icon" aria-hidden="true"></span>
           <span className="sr-only">Previous</span>
         </a>
+        <a className="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+          <span className="carousel-control-next-icon" aria-hidden="true"></span>
+          <span className="sr-only">Next</span>
+        </a>
       </div>
     );
   }
