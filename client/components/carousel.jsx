c46f02af4cef420c7bf035b40d85fcc7b8c94f8d
blake-ros <dev@MSI.localdomain>
2020-04-15 10:46:42 -0700

    added first slide to carousel component

--- client/components/carousel.jsx
+++ client/components/carousel.jsx
@@ -3,7 +3,18 @@ import React from 'react';
 class Carousel extends React.Component {
   render() {
     return (
-      <div className="container-fluid">Stuff Here</div>
+      <div id="carouselExampleIndicators" className="carousel slide" data-ride="carousel">
+        <ol className="carousel-indicators">
+          <li data-target="#carouselExampleIndicators" data-slide-to="0" className="active"></li>
+          <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+          <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+        </ol>
+        <div className="carousel-inner">
+          <div className="carousel-item active">
+            <img className="d-block w-100" src="#" alt="First slide"></img>
+          </div>
+        </div>
+      </div>
     );
   }
 }
