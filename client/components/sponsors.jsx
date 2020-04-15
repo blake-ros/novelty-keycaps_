78a892085669ade2b35590dd8b41a30eb2ecb940
blake-ros <dev@MSI.localdomain>
2020-04-14 20:22:14 -0700

    changed sponsors styling

--- client/components/sponsors.jsx
+++ client/components/sponsors.jsx
@@ -3,13 +3,14 @@ import React from 'react';
 class Sponsors extends React.Component {
   render(props) {
     return (
-      <div className="container-fluid pt-3">
+      <div className="page-header pt-3">
         <h1 className="text-center testimonial">Sponsors</h1>
         <div className="container-fluid d-flex justify-content-center">
           <div className="row justify-content-between align-items-center">
             <div className="col-3"><img src="./images/ducky_thumb.png"></img></div>
             <div className="col-3"><img src="./images/vortex_thumb.png"></img></div>
             <div className="col-3"><img src="./images/varmilo_new2_thumb.png"></img></div>
+            <div className="col-3"><img src="./images/leopold_thumb.png"></img></div>
           </div>
         </div>
       </div>
