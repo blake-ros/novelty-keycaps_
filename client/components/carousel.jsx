e1755af965b10b35bbc6d5c5f8ef4a385ebff5ec
blake-ros <dev@MSI.localdomain>
2020-04-14 20:52:49 -0700

    created new carousel component

---
+++ client/components/carousel.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Carousel extends React.Component {
+  render() {
+    return (
+      <div className="container-fluid">Stuff Here</div>
+    );
+  }
+}
+
+export default Carousel;
