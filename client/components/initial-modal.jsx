cbcb52ee5dc77d3558918c85a575618d8aebe4e6
blake-ros <dev@MSI.localdomain>
2020-04-16 14:44:07 -0700

    changed inital modal message

--- client/components/initial-modal.jsx
+++ client/components/initial-modal.jsx
@@ -7,10 +7,10 @@ function InitialModal(props) {
     <div className={`position-fixed h-100 w-100 overlay ${props.showInitialModal.displayNone ? 'd-none' : 'd-flex'} ${props.showInitialModal.show ? 'fade-in' : 'fade-out'}`}>
       <div className="m-auto p-3">
         <div className={`bg-white rounded p-3 modal-message ${props.showIntitialModal ? 'slide-in' : 'slide-out'}`}>
-          <h5 className="text-center">Welcome to Coffeine Supply Co</h5>
+          <h5 className="text-center">Novelty Keycaps_ Demo</h5>
           <div className="d-flex">
           </div>
-          <p className="text-center">Coffeine Supply Co is a LAMP stack content management app that was created strictly for demonstration purposes.  By clicking the button below, you accept that no purchases will be made, no payment processing will be done, and that actual personal information should not be used in checkout, such as real names, addresses, or credit card numbers.</p>
+          <p className="text-center">Welcome to my e-commerce webpage Novelty Keycaps_ this website is a demo and no real purchases will be made. Please refrain from using your personal information!</p>
           <div className="btn-group w-100">
             <button className="btn btn-danger w-100"
               onClick={() => props.toggleInitialModal()}>Accept</button>
