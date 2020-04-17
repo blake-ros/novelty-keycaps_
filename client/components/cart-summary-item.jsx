9d5eab5f13b6de48973d886b9591d5f83ec6dcb4
blake-ros <dev@MSI.localdomain>
2020-04-16 20:52:23 -0700

    created icons and quantity box for the quantity addition and subtracting of items in the cart state

--- client/components/cart-summary-item.jsx
+++ client/components/cart-summary-item.jsx
@@ -8,6 +8,27 @@ class CartSummaryItem extends React.Component {
     };
   }
 
+  removeItemModal() {
+    return (
+      <div className={`position-fixed h-100 w-100 ${this.props.showModal.displayNone ? 'd-none' : 'd-flex'} overlay ${this.props.showModal.show ? 'fade-in' : 'fade-out'}`}>
+        <div className="m-auto p-4">
+          <div className={`bg-white rounded p-3 modal-message ${this.props.showModal.show ? 'slide-in' : 'slide-out'}`}>
+            <div className="d-flex">
+              <i className="far fa-times-circle cancel-button ml-auto"
+                onClick={this.props.toggleModal} />
+            </div>
+            <h5 className="text-center">New Item Added</h5>
+            <p className="text-center">This item is now in your cart.</p>
+            <div>
+              <button className="btn btn-secondary mr-2 mb-4" onClick={() => this.props.newView('catalog', {})}>Continue Shopping</button>
+              <button className="btn btn-primary text-white ml-2 mb-4" onClick={() => this.props.newView('cart', {})}>View Cart</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+
   render() {
     const cartItem = this.props.cartItem;
 
@@ -22,6 +43,16 @@ class CartSummaryItem extends React.Component {
               <h2 className="card-title">{cartItem.name}</h2>
               <span className="card-text text-secondary mb-5">${(cartItem.price * 0.01).toFixed(2)}</span>
               <p className="card-text mt-3">{cartItem.shortDescription}</p>
+              <div className="d-flex">
+                <div className="d-flex align-items-center justify-content-center">
+                  <i className="fas fa-minus"></i>
+                </div>
+                <input type="number" className="text-center" value={this.state.quantity} />
+                <div className="d-flex align-items-center justify-content-center">
+                  <i className="fas fa-plus"></i>
+                </div>
+              </div>
+              <button onClick={this.removeItemModal} className="btn btn-danger mt-2">Remove</button>
             </div>
           </div>
         </div>
