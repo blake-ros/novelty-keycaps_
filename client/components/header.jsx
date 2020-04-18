78cac2c73e57782908a9ee241cd45c4949f9e2e4
blake-ros <dev@MSI.localdomain>
2020-04-18 12:29:05 -0700

    moved carousel into final position, fixed header background color

--- client/components/header.jsx
+++ client/components/header.jsx
@@ -5,9 +5,9 @@ class Header extends React.Component {
   render(props) {
     const testimonial = 'The Ultimate Keycap Catalog';
     return (
-      <div className="container">
-        <h1 className="text-center pb-5 pt-5">Novelty Keycaps_<div><h6 className="text-center testimonial">&quot;{testimonial}&quot;</h6></div></h1>
-        <div className="d-flex justify-content-end col-12"><h2 className="text-right pointer col-xl-2 col-lg-3 col-md-4 col-sm-4 col-xs-4 pr-4" onClick={() => this.props.onRender('cart', {})}><i className="fas fa-shopping-cart"></i> {this.props.cartItemCount}</h2></div>
+      <div className="container-fluid pt-4 bg-light">
+        <h1 className="text-center">Novelty Keycaps_<h6 className="text-center testimonial">&quot;{testimonial}&quot;</h6></h1>
+        <h2 className="d-flex justify-content-end pointer mr-4" onClick={() => this.props.onRender('cart', {})}><i className="fas fa-shopping-cart mr-2 mt-1"></i> {this.props.cartItemCount}</h2>
         <InitialModal showInitialModal={this.props.showInitialModal} toggleInitialModal={this.props.toggleInitialModal} />
       </div>
     );
