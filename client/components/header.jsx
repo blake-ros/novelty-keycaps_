68656a525962ed4583cca9879a90520bb8e6f63b
blake-ros <dev@MSI.localdomain>
2020-04-18 13:04:23 -0700

    fixed header react dom error, also completed the header and footer styling along with background styling

--- client/components/header.jsx
+++ client/components/header.jsx
@@ -6,11 +6,12 @@ class Header extends React.Component {
     const testimonial = 'The Ultimate Keycap Catalog';
     return (
       <div className="container-fluid pt-4 headerBackground">
-        <h1 className="text-center">Novelty Keycaps_<h6 className="text-center testimonial">&quot;{testimonial}&quot;</h6></h1>
+        <h1 className="text-center">Novelty Keycaps_</h1>
+        <h6 className="text-center testimonial">&quot;{testimonial}&quot;</h6>
         <div className="row">
           <h2 className=" col-12 text-right pointer mr-4 pr-5" onClick={() => this.props.onRender('cart', {})}><i className="fas fa-shopping-cart mr-1 mt-1"></i> {this.props.cartItemCount}</h2>
-          <InitialModal showInitialModal={this.props.showInitialModal} toggleInitialModal={this.props.toggleInitialModal} />
         </div>
+        <InitialModal showInitialModal={this.props.showInitialModal} toggleInitialModal={this.props.toggleInitialModal} />
       </div>
     );
   }
