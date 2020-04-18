65e763680429bc3c790108b10bab0bde84b4bbc2
blake-ros <dev@MSI.localdomain>
2020-04-18 13:43:57 -0700

    adding padding to cart summary page

--- client/components/cart-summary.jsx
+++ client/components/cart-summary.jsx
@@ -18,7 +18,7 @@ class CartSummary extends React.Component {
         <button className="btn btn-info mt-3" onClick={() => this.props.onRender('catalog', {})}>Back to Catalog</button>
         {itemsInCart}
         {cartArray.map(item => { return <CartSummaryItem key={item.cartItemId} cartItem={item}/>; })}
-        <h4 className="text-secondary">Item Total: ${cartTotal} {button}</h4>
+        <h4 className="text-secondary mb-3">Item Total: ${cartTotal} {button}</h4>
       </div>
     );
   }
