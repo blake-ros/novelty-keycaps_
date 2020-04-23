2f890892033c8942c31d935263d9a831649c3f18
blake-ros <dev@MSI.localdomain>
2020-04-22 17:28:18 -0700

    updated cart summary to render the total instead of the item total

--- client/components/cart-summary.jsx
+++ client/components/cart-summary.jsx
@@ -18,7 +18,7 @@ class CartSummary extends React.Component {
         <button className="btn btn-info mt-3" onClick={() => this.props.onRender('catalog', {})}>Back to Catalog</button>
         {itemsInCart}
         {cartArray.map(item => { return <CartSummaryItem key={item.cartItemId} cartItem={item} removeItem={this.props.removeItem} cart={this.props.cart} />; })}
-        <h4 className="text-secondary mb-3">Item Total: ${cartTotal} {button}</h4>
+        <h4 className="text-secondary mb-3">Total: ${cartTotal} {button}</h4>
       </div>
     );
   }
